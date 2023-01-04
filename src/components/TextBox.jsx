50f81c31b26e0a3d6795a38d99d63aa23558e384
OfficialKiranMahajan <officialkiranmahajan@gmail.com>
2023-01-04 20:12:35 +0530

    button changes and added some new functions

--- src/components/TextBox.jsx
+++ src/components/TextBox.jsx
@@ -1,10 +1,24 @@
 import React, { useState } from "react";
 
 const TextBox = () => {
-  const toUppercase = () => {
+  const inputToUpperCase = () => {
     let userInput = text.toUpperCase();
     setText(userInput);
   };
+   const inputToLowerCase = () => {
+     let userInput = text.toLowerCase();
+     setText(userInput);
+   };
+   const inputToCLear = () => {
+     let userInput = "";
+     setText(userInput);
+   };
+   const inputToClipboard = () => {
+     let userInput = "";
+     setText(userInput);
+   };
+
+
   const handleOnChange = (event) => {
     setText(event.target.value);
   };
@@ -20,13 +34,39 @@ const TextBox = () => {
           className="relative mt-10 text-white bg-gray-600 w-[45rem] h-64 p-2 border-lime-100 rounded-lg focus:outline-none focus:shadow-outline-blue-500"
         ></textarea>
       </div>
-      <div className="btn flex justify-around mt-4  ">
-        <button
-          onClick={toUppercase}
-          className="bg-gray-900 text-green-400 p-1 px-3 rounded-md hover:text-gray-900 hover:bg-green-400 font-semibold transition-all duration-400 ease-linear active:bg-gray-600 active:text-green-200 active:scale-90 "
+      <div className="btn flex space-x-4 mt-4  ">
+        <a
+          onClick={inputToUpperCase}
+          href="#_"
+          class="relative inline-flex items-center justify-start px-4 py-2 overflow-hidden font-semibold transition-all bg-green-400 rounded-md hover:bg-gray-900  active:scale-95 group"
         >
-          Uppercase
-        </button>
+          <span class="w-48 h-48 rounded rotate-[-40deg] bg-gray-900 absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
+          <span class="relative w-full text-left text-gray-900 transition-colors duration-300 ease-in-out group-hover:text-green-400">
+            UpperCase
+          </span>
+        </a>
+
+        <a
+          onClick={inputToLowerCase}
+          href="#_"
+          class="relative inline-flex items-center justify-start px-4 py-2 overflow-hidden font-semibold transition-all bg-green-400 rounded-md hover:bg-gray-900  active:scale-95 group"
+        >
+          <span class="w-48 h-48 rounded rotate-[-40deg] bg-gray-900 absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
+          <span class="relative w-full text-left text-gray-900 transition-colors duration-300 ease-in-out group-hover:text-green-400">
+            LowerCase
+          </span>
+        </a>
+
+        <a
+          onClick={inputToCLear}
+          href="#_"
+          class="relative inline-flex items-center justify-start px-4 py-2 overflow-hidden font-semibold transition-all bg-green-400 rounded-md hover:bg-gray-900  active:scale-95 group"
+        >
+          <span class="w-48 h-48 rounded rotate-[-40deg] bg-gray-900 absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
+          <span class="relative w-full text-left text-gray-900 transition-colors duration-300 ease-in-out group-hover:text-green-400">
+            Clear
+          </span>
+        </a>
       </div>
       <div className="summary mt-3">
         <p>
