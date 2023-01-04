a5efa045bdcb3027e50fb7e9961bc1bf83aa3cb5
OfficialKiranMahajan <officialkiranmahajan@gmail.com>
2023-01-04 20:49:23 +0530

    added some more dummy buttons

--- src/components/TextManipulator.jsx
+++ src/components/TextManipulator.jsx
@@ -17,6 +17,10 @@ const TextBox = () => {
      let userInput = "";
      setText(userInput);
    };
+ const inputToCamelCase = () => {
+   let userInput = "";
+   setText(userInput);
+ };
 
 
   const handleOnChange = (event) => {
@@ -31,7 +35,7 @@ const TextBox = () => {
           value={text}
           id="Textbox"
           rows="10"
-          className="relative mt-10 border-2 max-h-96 border-green-400 text-white bg-gray-600 w-[60rem] max-w-screen-lg h-64 p-2 border-lime-100 rounded-lg focus:outline-none focus:shadow-outline-blue-500"
+          className="relative mt-10 border-2 max-h-72 min-h-0 border-green-400 text-white bg-gray-600 w-[55rem] max-w-screen-lg h-64 p-2 border-lime-100 rounded-lg focus:outline-none focus:shadow-outline-blue-500"
         ></textarea>
       </div>
       <div className="btn flex space-x-4 mt-4  ">
@@ -58,6 +62,47 @@ const TextBox = () => {
         </a>
 
         <a
+          onClick={inputToCamelCase}
+          href="#_"
+          class="relative inline-flex items-center justify-start px-4 py-2 overflow-hidden font-semibold transition-all bg-green-400 rounded-md hover:bg-gray-900  active:scale-95 group"
+        >
+          <span class="w-48 h-48 rounded rotate-[-40deg] bg-gray-900 absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
+          <span class="relative w-full text-left text-gray-900 transition-colors duration-300 ease-in-out group-hover:text-green-400">
+            CamelCase
+          </span>
+        </a>
+        <a
+          onClick={inputToFirstLetterCapital}
+          href="#_"
+          class="relative inline-flex items-center justify-start px-4 py-2 overflow-hidden font-semibold transition-all bg-green-400 rounded-md hover:bg-gray-900  active:scale-95 group"
+        >
+          <span class="w-48 h-48 rounded rotate-[-40deg] bg-gray-900 absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
+          <span class="relative w-full text-left text-gray-900 transition-colors duration-300 ease-in-out group-hover:text-green-400">
+            First Letter Capital
+          </span>
+        </a>
+        <a
+          onClick={inputToRemoveExtraSpace}
+          href="#_"
+          class="relative inline-flex items-center justify-start px-4 py-2 overflow-hidden font-semibold transition-all bg-green-400 rounded-md hover:bg-gray-900  active:scale-95 group"
+        >
+          <span class="w-48 h-48 rounded rotate-[-40deg] bg-gray-900 absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
+          <span class="relative w-full text-left text-gray-900 transition-colors duration-300 ease-in-out group-hover:text-green-400">
+            Remove Extra Spaces
+          </span>
+        </a>
+         <a
+          onClick={inputToUnderscodeAdder}
+          href="#_"
+          class="relative inline-flex items-center justify-start px-4 py-2 overflow-hidden font-semibold transition-all bg-green-400 rounded-md hover:bg-gray-900  active:scale-95 group"
+        >
+          <span class="w-48 h-48 rounded rotate-[-40deg] bg-gray-900 absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
+          <span class="relative w-full text-left text-gray-900 transition-colors duration-300 ease-in-out group-hover:text-green-400">
+            Replace Spaces With Underscodes
+          </span>
+        </a>
+
+        <a
           onClick={inputToCLear}
           href="#_"
           class="relative inline-flex items-center justify-start px-4 py-2 overflow-hidden font-semibold transition-all bg-green-400 rounded-md hover:bg-gray-900  active:scale-95 group"
@@ -67,6 +112,38 @@ const TextBox = () => {
             Clear
           </span>
         </a>
+        <a
+          onClick={inputToClipboard}
+          href="#_"
+          class="relative inline-flex items-center justify-start px-4 py-2 overflow-hidden font-semibold transition-all bg-green-400 rounded-md hover:bg-gray-900  active:scale-95 group"
+        >
+          <span class="w-48 h-48 rounded rotate-[-40deg] bg-gray-900 absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
+          <span class="relative w-full text-left text-gray-900 transition-colors duration-300 ease-in-out group-hover:text-green-400">
+            Copy
+          </span>
+        </a> 
+        <a
+          onClick={inputToCut}
+          href="#_"
+          class="relative inline-flex items-center justify-start px-4 py-2 overflow-hidden font-semibold transition-all bg-green-400 rounded-md hover:bg-gray-900  active:scale-95 group"
+        >
+          <span class="w-48 h-48 rounded rotate-[-40deg] bg-gray-900 absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
+          <span class="relative w-full text-left text-gray-900 transition-colors duration-300 ease-in-out group-hover:text-green-400">
+           Cut
+          </span>
+        </a>
+        <a
+          onClick={inputToPaste}
+          href="#_"
+          class="relative inline-flex items-center justify-start px-4 py-2 overflow-hidden font-semibold transition-all bg-green-400 rounded-md hover:bg-gray-900  active:scale-95 group"
+        >
+          <span class="w-48 h-48 rounded rotate-[-40deg] bg-gray-900 absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
+          <span class="relative w-full text-left text-gray-900 transition-colors duration-300 ease-in-out group-hover:text-green-400">
+           Paste
+          </span>
+        </a>
+
+
       </div>
       <div className="summary mt-3">
         <p>
